8a7081c6581f1910d3b96b79a0d1f6b403ebd152
HigorJesus <102111394+HigorJesus@users.noreply.github.com>
2023-03-09 19:23:31 -0300

    condições

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel =100
+console.log(`sua velocidade é de ${vel} km/h`)
+if(vel >60){//condição simples por que ela não tem se não 
+    console.log(`você ultrapassou a velocidade máxima MULTADO!`)
+}
+console.log(`dirija sempre com o cinto de segurança`)

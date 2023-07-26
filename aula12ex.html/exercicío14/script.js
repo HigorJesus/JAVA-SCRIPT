a1a70138140cb743644710022b8a4a8cd676f096
HigorJesus <102111394+HigorJesus@users.noreply.github.com>
2023-07-26 15:25:59 -0300

    horas do dia

--- aula12ex.html/exercicío14/script.js
+++ aula12ex.html/exercicío14/script.js
@@ -3,16 +3,20 @@ function carregar(){
     var img=window.document.getElementById('imagem')
     var data= new Date()
     var hora= data.getHours()
-    msg.innerHTML= `Agora são ${hora} hora`
+
+    msg.innerHTML= `Agora são ${hora} horas`
     if(hora >= 0 && hora < 12 ){
         //BOM DIA!
         img.src = 'manha.png'
+        document.body.style.background= '#e2cd94'
     } else if ( hora >= 12 && hora < 18){
         //BOA TARDE!
         img.src = 'tarde.png'
+        document.body.style.background='#b9846f'
     } else{
         //BOA NOITE!
         img.src = 'noite.png'
+        document.body.style.background='#515154'
     }
 }
     

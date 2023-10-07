2732d399dcc3bdef48629c13c1989ccde6cbf4a8
Higor Jesus <higorjsus@gmail.com>
2023-10-07 12:26:42 -0300

    estrutura de repetição

---
+++ aula13/ambinte.js
@@ -0,0 +1,28 @@
+
+/*
+
+Estrutura de repetição no inicio
+
+
+var c = 1 
+while (c < 5) {
+    console.log(`passo ${c}`);
+    c++
+
+
+
+}*/
+
+ 
+var c = 1
+do{
+    console.log(c)
+    c++
+}while( c < 6)
+
+/*
+
+Estrutura de repetição no final
+
+
+*/
